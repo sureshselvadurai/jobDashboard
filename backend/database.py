41f2733bc1539c016f0d541f92982830f82925c0
SureshRajaMH <sselvadurai@selectdr.com>
2025-05-03 19:18:55 -0700

    -- WF

--- backend/database.py
+++ backend/database.py
@@ -43,6 +43,7 @@ def wait_for_db(engine, retries=10, delay=3):
             time.sleep(delay)
     raise RuntimeError("❌ Database not ready after retries.")
 
+
 def init_db():
     print("Database User:", Config.DB_USER)
     wait_for_db(engine)
